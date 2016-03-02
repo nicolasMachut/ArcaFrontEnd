ba0946106e1f9534def5456df7c645ec8540da72
Nicolas MACHUT <Nicolas MACHUT>
2016-03-02 18:58:01 +0100

    initial commit

---
+++ app/scripts/controllers/about.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name arcaFrontEndApp.controller:AboutCtrl
+ * @description
+ * # AboutCtrl
+ * Controller of the arcaFrontEndApp
+ */
+angular.module('arcaFrontEndApp')
+  .controller('AboutCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
---
+++ app/scripts/controllers/main.js
@@ -0,0 +1,17 @@
+'use strict';
+
+/**
+ * @ngdoc function
+ * @name arcaFrontEndApp.controller:MainCtrl
+ * @description
+ * # MainCtrl
+ * Controller of the arcaFrontEndApp
+ */
+angular.module('arcaFrontEndApp')
+  .controller('MainCtrl', function () {
+    this.awesomeThings = [
+      'HTML5 Boilerplate',
+      'AngularJS',
+      'Karma'
+    ];
+  });
