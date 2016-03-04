b0a60e008cf52841c189fce67c41cdf8a1060f54
nicolasMachut <machut.nicolas@gmail.com>
2016-03-04 12:18:19 +0100

    add chart

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -1,6 +1,17 @@
 'use strict';
 
-var app = angular.module('arcaFrontEndApp', ['ngResource']);
+var app = angular.module('arcaFrontEndApp', ['ngResource', 'chart.js']);
+
+app.config(['ChartJsProvider', function (ChartJsProvider) {
+  ChartJsProvider.setOptions({
+    colours: ['#FF5252', '#FF8A80'],
+    responsive: true
+  });
+  // Configure all line charts
+  ChartJsProvider.setOptions('Line', {
+    datasetFill: false
+  });
+}]);
 
 app.factory("Post", function($resource) {
   return $resource("http://localhost:8080/entry-point/test");
@@ -8,12 +19,33 @@ app.factory("Post", function($resource) {
 
 app.controller('batchController', ['$scope', 'Post', function ($scope, Post) {
   $scope.test = Post.query();
-  console.log($scope.test);
+  $scope.onClick = function () {
+    // Effectuer requete vers web service
+    console.log("demarrage");
+  };
 }]);
 
 app.controller('tabController', ['$scope', function ($scope) {
   $scope.lines = [{country: "Belgique", value: 190}, {country: "France", value: 180}];
 }]);
 
+app.controller("chartController", function ($scope, $timeout) {
 
+  $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
+  $scope.series = ['Series A', 'Series B'];
+  $scope.data = [
+    [65, 59, 80, 81, 56, 55, 40],
+    [28, 48, 40, 19, 86, 27, 90]
+  ];
+  $scope.onClick = function (points, evt) {
+    console.log(points, evt);
+  };
 
+  // Simulate async data update
+  $timeout(function () {
+    $scope.data = [
+      [28, 48, 40, 19, 86, 27, 90],
+      [65, 59, 80, 81, 56, 55, 40]
+    ];
+  }, 3000);
+});
