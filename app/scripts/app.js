90a491feb8ee350d4724c633d91976df2227dc53
nicolasMachut <machut.nicolas@gmail.com>
2016-03-04 13:05:27 +0100

    add next and previous year

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -33,7 +33,22 @@ app.controller('tabController', ['$scope', function ($scope) {
 
 app.controller("chartController", function ($scope, $timeout) {
 
-  $scope.annee = 2016;
+  $scope.year = 2016;
+  $scope.nextYear = function () {
+    $scope.year +=1;
+    $scope.data = [
+      [65, 59, 80, 81, 56, 55, 40],
+      [28, 48, 40, 19, 86, 27, 90]
+    ];
+  };
+  $scope.previousYear = function () {
+    $scope.year -=1;
+    $scope.data = [
+      [28, 48, 40, 19, 86, 27, 90],
+      [65, 59, 80, 81, 56, 55, 40]
+    ];
+  };
+
   $scope.labels = ["January", "February", "March", "April", "May", "June", "July"];
   $scope.series = ['Series A', 'Series B'];
   $scope.data = [
@@ -43,12 +58,4 @@ app.controller("chartController", function ($scope, $timeout) {
   $scope.onClick = function (points, evt) {
     console.log(points, evt);
   };
-
-  // Simulate async data update
-  $timeout(function () {
-    $scope.data = [
-      [28, 48, 40, 19, 86, 27, 90],
-      [65, 59, 80, 81, 56, 55, 40]
-    ];
-  }, 3000);
 });
