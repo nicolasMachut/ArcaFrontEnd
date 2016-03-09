c92ddc9df41ab529ee4b97bad5f4ebdfae165f32
Nicolas Machut <machut.nicolas@gmail.com>
2016-03-09 18:57:41 +0100

    add travis

--- app/scripts/app.js
+++ app/scripts/app.js
@@ -3,95 +3,70 @@
 var app = angular.module('arcaFrontEndApp', ['ngResource', 'chart.js']);
 
 app.config(['ChartJsProvider', function (ChartJsProvider) {
+
+  // Define chart's options
+  // Line colour and responsive to true
   ChartJsProvider.setOptions({
     colours: ['#337ab7', '#5bc0de'],
     responsive: true
   });
-  // Configure all line charts
-  ChartJsProvider.setOptions('Line', {
-    datasetFill: false
-
-  });
 }]);
 
-app.factory("getChartsData", function($resource) {
-  return {
-    statsOfTheYear: function (year){
-      //return $resource("https://secure-caverns-39897.herokuapp.com/lines/chart/:year", {year:year}).query()
-      return [   [65, 59]      ];
-    },
 
-    valuesByCountry: function () {
-      return $resource("localhost:8080/entry-point/test").get();
-    }
-  }
-});
-
-app.factory("batchManager", function($resource) {
-  return {
-    launchBatch: function (){
-      return "ok";
-    }
-  }
-});
-
-app.controller('batchController', function ($scope, batchManager) {
+app.controller('batchController', function ($scope) {
 
   $scope.inProgress = false;
   $scope.action = "Lancer le traitement";
-
   $scope.state = 30;
-  //$scope.test = Post.query();
+
   $scope.launchBatch = function () {
-    batchManager.launchBatch();
+    //TODO : Request web service to start the batch
+
     $scope.inProgress = true;
     $scope.action = "Traitement en cours";
   };
+
+  // TODO : Connect to web service with websocket to get batch state
 });
 
-app.controller('tabController', function ($scope, getChartsData, $resource) {
+app.controller('tabController', function ($scope, $resource) {
+
+  // Fetch values grouped by by country
   $scope.lines = $resource("https://secure-caverns-39897.herokuapp.com/lines/lineByCountry").query();
 });
 
-app.controller("chartController", function ($scope, getChartsData, $resource) {
+app.controller("chartController", function ($scope, $resource, $filter) {
 
   $scope.year = 2008;
+  $scope.data = [];
+  loadData();
+
+  function loadData (){
+    $scope.series = [$scope.year];
+    $scope.labels = [];
+    $scope.data[0] = [];
+    var res = $resource("https://secure-caverns-39897.herokuapp.com/lines/chart/:year", {year:$scope.year}).query().$promise.then(function (result){
+      displayData(result);
+    });
+  }
+
+  function displayData(result) {
+    $scope.showChart = result.length != 0;
+    angular.forEach(result, function (value, key){
+      var day = value._id;
+      var date = new Date($scope.year, 0); // initialize a date in `year-01-01`
+      var formattedDate = new Date(date.setDate(day));
+      $scope.labels.push($filter('date')(formattedDate, "dd/MM"));
+      $scope.data[0].push(parseInt(value.sum));
+    })
+  }
 
   $scope.nextYear = function () {
     $scope.year +=1;
-    //$scope.data = getChartsData.statsOfTheYear($scope.year);
-    var res = $resource("https://secure-caverns-39897.herokuapp.com/lines/chart/:year", {year:$scope.year}).query().$promise.then(function (result){
-      $scope.data[0] = [];
-      angular.forEach(result, function (value, key){
-        $scope.labels.push(value._id);
-        $scope.data[0].push(parseInt(value.sum));
-      })
-    });
+    loadData();
   };
   $scope.previousYear = function () {
     $scope.year -=1;
-    //$scope.data = getChartsData.statsOfTheYear($scope.year);
-    var res = $resource("https://secure-caverns-39897.herokuapp.com/lines/chart/:year", {year:$scope.year}).query().$promise.then(function (result){
-      $scope.data[0] = [];
-      angular.forEach(result, function (value, key){
-        $scope.labels.push(value._id);
-        $scope.data[0].push(parseInt(value.sum));
-      })
-    });
+    loadData();
   };
-
-  $scope.data = getChartsData.statsOfTheYear($scope.year);
-  $scope.labels = [];
-  $scope.series = ['Series A'];
-  $scope.onClick = function (points, evt) {
-    console.log(points, evt);
-  };
-
-  var res = $resource("https://secure-caverns-39897.herokuapp.com/lines/chart/:year", {year:2008}).query().$promise.then(function (result){
-    $scope.data[0] = [];
-    angular.forEach(result, function (value, key){
-      $scope.labels.push(value._id);
-      $scope.data[0].push(parseInt(value.sum));
-    })
-  });
 });
